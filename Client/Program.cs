9f544c66a29cfec5b063d4c946209b99026e7365
Stewart Celani <me@stewartcelani.com>
2022-04-10 16:17:08 +0930

    Step 4: ServerConsole.razor, DebugService and hooking them up to the index page

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WasmLogToBrowser.Client;
+using WasmLogToBrowser.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DebugService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/DebugService.cs
@@ -0,0 +1,23 @@
+﻿namespace WasmLogToBrowser.Client.Services
+{
+    public class DebugService
+    {
+        public event Func<Task>? OnChange;
+        public bool ConnectToServerConsole = true;
+        public List<string> LogMessages = new List<string>();
+
+        public void AddLogMessage(string logMessage)
+        {
+            LogMessages.Add(logMessage);
+            HandleOnChange();
+        }
+
+        private void HandleOnChange()
+        {
+            if (OnChange != null)
+            {
+                OnChange?.Invoke();
+            }
+        }
+    }
+}
