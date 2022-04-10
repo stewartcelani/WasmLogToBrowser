cde96d6516bbcba8b5b2d5cd2f6b824dc49f063c
Stewart Celani <me@stewartcelani.com>
2022-04-10 16:56:56 +0930

    Step 5: Make the console toggle-able and implement clearing the console - Part 1

--- Client/Services/DebugService.cs
+++ Client/Services/DebugService.cs
@@ -3,12 +3,18 @@
     public class DebugService
     {
         public event Func<Task>? OnChange;
-        public bool ConnectToServerConsole = true;
+        public bool ConnectToServerConsole = false;
         public List<string> LogMessages = new List<string>();
 
         public void AddLogMessage(string logMessage)
         {
             LogMessages.Add(logMessage);
+            HandleOnChange();
+        }
+
+        public void ToggleConnectToServerConsole()
+        {
+            ConnectToServerConsole = !ConnectToServerConsole;
             HandleOnChange();
         }
 
